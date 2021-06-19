243811bc324d93b04a41b14664890b9da49effd9
​ <>
2021-06-18 20:00:27 -0400

    load sourcemaps in chromium

--- build/webpack.config-consts.js
+++ build/webpack.config-consts.js
@@ -1,6 +1,7 @@
 export default {
 	mode: process.env.NODE_ENV,
-	devtool: process.env.NODE_ENV === "development" ? "cheap-source-map" : false,
+	// "eval-source-map" as workaround so that Chromium can load sourcemaps (https://stackoverflow.com/questions/54669476/chrome-72-changes-sourcemap-behaviour)
+	devtool: process.env.NODE_ENV === "development" ? "eval-source-map" /* "cheap-source-map" */ : false,
 	resolve: {
 		alias: {
 			"@lib": "/lib",
