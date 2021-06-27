b93eb186c5e862186aaa9602c70a275ea05ed129
​ <>
2021-06-26 22:13:45 -0400

    add basic options display

--- webext/popup/components/OptionsControls.vue
+++ webext/popup/components/OptionsControls.vue
@@ -1,11 +1,36 @@
 <template>
-	<options-controls>
-		<p>{{engineOptions}}</p>
+	<options-controls @input="setOptions">
+		<table>
+			<tr>
+				<th>Loudness threshold (<abbr title="decibels, relative to maximum amplitude">dBFS</abbr>)</th>
+				<td>
+					<input type="range" min="0" max="1" step="any"/>
+					<input type="text" v-model.number="engineOptions.thresholdAmp" />
+
+					{{thresholdDbfs}}
+				</td>
+			</tr>
+			<tr>
+				<th>Loud playback speed</th>
+				<td>
+					<input type="range" min="-2" max="2" step="any" />
+					<input type="text" v-model.number="engineOptions.loudSpeed" />
+				</td>
+			</tr>
+			<tr>
+				<th>Quiet playback speed</th>
+				<td>
+					<input type="range" min="-2" max="2" step="any" />
+					<input type="text" v-model.number="engineOptions.softSpeed" />
+				</td>
+			</tr>
+		</table>
 	</options-controls>
 </template>
 
 <script>
 import contentScriptPromise from "../ContentComm.js";
+import ExtremaAnalyser from "@lib/volume-calc/ExtremaAnalyser.js";
 
 let Content;
 
@@ -17,6 +42,20 @@ export default {
 
 		engineOptions: null,
 	}),
+
+	methods: {
+		async setOptions() {
+			await Content.setEngineOptions(this.engineOptions);
+
+			console.log(await Content.getEngineOptions());
+		},
+	},
+
+	computed: {
+		thresholdDbfs() {
+			return ExtremaAnalyser.dbfsFromAmp(this.engineOptions.thresholdAmp);
+		},
+	},
 
 	async created() {
 		Content = await contentScriptPromise;
